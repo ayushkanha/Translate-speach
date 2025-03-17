e705d9f05770b1d7b8e8f1b2129b5107237b861f
ayushkanha <61038773+ayushkanha@users.noreply.github.com>
2025-03-17 13:14:43 +0530

    Update translate_speach.py

--- translate_speach.py
+++ translate_speach.py
@@ -67,13 +67,16 @@ def transcribe(uploaded_file):
 Languages = {'afrikaans':'af','albanian':'sq','amharic':'am','arabic':'ar','armenian':'hy','azerbaijani':'az','basque':'eu','belarusian':'be','bengali':'bn','bosnian':'bs','bulgarian':'bg','catalan':'ca','cebuano':'ceb','chichewa':'ny','chinese (simplified)':'zh-cn','chinese (traditional)':'zh-tw','corsican':'co','croatian':'hr','czech':'cs','danish':'da','dutch':'nl','english':'en','esperanto':'eo','estonian':'et','filipino':'tl','finnish':'fi','french':'fr','frisian':'fy','galician':'gl','georgian':'ka','german':'de','greek':'el','gujarati':'gu','haitian creole':'ht','hausa':'ha','hawaiian':'haw','hebrew':'iw','hebrew':'he','hindi':'hi','hmong':'hmn','hungarian':'hu','icelandic':'is','igbo':'ig','indonesian':'id','irish':'ga','italian':'it','japanese':'ja','javanese':'jw','kannada':'kn','kazakh':'kk','khmer':'km','korean':'ko','kurdish (kurmanji)':'ku','kyrgyz':'ky','lao':'lo','latin':'la','latvian':'lv','lithuanian':'lt','luxembourgish':'lb','macedonian':'mk','malagasy':'mg','malay':'ms','malayalam':'ml','maltese':'mt','maori':'mi','marathi':'mr','mongolian':'mn','myanmar (burmese)':'my','nepali':'ne','norwegian':'no','odia':'or','pashto':'ps','persian':'fa','polish':'pl','portuguese':'pt','punjabi':'pa','romanian':'ro','russian':'ru','samoan':'sm','scots gaelic':'gd','serbian':'sr','sesotho':'st','shona':'sn','sindhi':'sd','sinhala':'si','slovak':'sk','slovenian':'sl','somali':'so','spanish':'es','sundanese':'su','swahili':'sw','swedish':'sv','tajik':'tg','tamil':'ta','telugu':'te','thai':'th','turkish':'tr','turkmen':'tk','ukrainian':'uk','urdu':'ur','uyghur':'ug','uzbek':'uz','vietnamese':'vi','welsh':'cy','xhosa':'xh','yiddish':'yi','yoruba':'yo','zulu':'zu'}
 c1, c2 = st.columns(2)
 
-inp = c1.selectbox('Choose Input Format', ("Text", "Audio"))
+inp = c1.selectbox('Choose Input Format', ("Text", "Audio_File","MIC"))
 text = ""
 
 if inp == "Text":
     text = c1.text_area("Enter Text:", help="Type your text here...")
+elif inp == "mic":
+    uploaded_file = c1.audio_input("Record a Voice Message")
+    if uploaded_file and c2.button('🎤 Transcribe Audio'):
+        text = transcribe(uploaded_file)
 else:
-    uploaded_file = c2.audio_input("Record a Voice Message")
     uploaded_file = c1.file_uploader("Upload an Audio File", type=["mp3", "wav", "m4a"])
     if uploaded_file and c2.button('🎤 Transcribe Audio'):
         text = transcribe(uploaded_file)
